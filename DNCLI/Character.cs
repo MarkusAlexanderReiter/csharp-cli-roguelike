5c41de91c9fdea01ae2499cf1c93a254676cb173
Reiter-Digital <info@reiter-digital.at>
2025-08-25 21:00:55 +0200

    added character cosntructor, rolling 3d6 for stats. print stats in main

--- DNCLI/Character.cs
+++ DNCLI/Character.cs
@@ -8,4 +8,17 @@ public class Character
     public int Intelligence { get; set; }
     public int Wisdom { get; set; }
     public int Charisma { get; set; }
+
+    public Character()
+    {
+        Random random = new Random();
+        Strength = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        Dexterity = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        Constitution = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        Intelligence = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        Wisdom = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        Charisma = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+
+
+    }
 }
--- DNCLI/Program.cs
+++ DNCLI/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace DNCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character player = new Character();
+            Console.WriteLine($"Player Strength: {player.Strength}");
+            Console.WriteLine($"Player Dexterity: {player.Dexterity}");
+            Console.WriteLine($"Player Constitution: {player.Constitution}");
+            Console.WriteLine($"Player Wisdom: {player.Wisdom}");
+            Console.WriteLine($"Player Intelligence: {player.Intelligence}");
+            Console.WriteLine($"Player Charisma: {player.Charisma}");
 
-Console.WriteLine("Hello, World!");
+        }
+    }
+}
